e33390f778ef0f86c4c9913d9961845c499c7745
Harry Rudolph <harry.rudolph.2020@uni.strath.ac.uk>
2020-11-05 12:05:13 +0000

    using bitwise operators baby

--- main.py
+++ main.py
@@ -5,77 +5,51 @@ def lengthOfListOfList(inputList):
     for i in inputList:
         for j in i:
             total += 1
-    
     return total
 
-def loadImageData(path):
+def loadImage(path):
     i = Image.open(path)
-    data = i.getdata()
+    imageData = i.getdata()
     i.close()
-    
-    return data
+    return imageData
 
-def stringToNumberList(string):
-    outputList = []
-    for i in string:
-        outputList.append(ord(i))
-    outputList.append(ord(" ")) #last number can sometimes become jumbled.
-    return outputList
+def payloadToBinaryList(payload):
+    characterList = []
+    payloadBitList = []
+    
+    for char in payload:
+        characterList.append(ord(char))
+    characterList.append(ord(" ")) #last number can sometimes become jumbled.
+    
+    #Add length of list to start.
+    payloadByteLength = len(characterList)
 
-def listToBytes(inList, maxLength): 
-    bitCount = 0
-    
-    outList = []
-    byte = ''
-    
-    for bit in range(maxLength): #TODO Need to think about how long the message is. It isn't helpful to store everything in memory. 
-        if bitCount < 8:
-            byte += str(inList[bit])
-            bitCount += 1
-        if bitCount == 8:
-            outList.append(byte)
-            byte = ''
-            bitCount = 0
-    return(outList)
-    
-def decode(path, maxLength): #Supposed to store length
-    data = loadImageData(path)
-    dataList = list(map(list, data)) #Python has funky tuple rules
-    extractedList = []
-    
-    for i in range(len(dataList)):
-        for rgb in range(3):
-            extractedList.append(dataList[i][rgb] & 1)
-            
-    byteList = listToBytes(extractedList, maxLength) # Can this be one loop
-    
-    message = ''
-    for i in range(len(byteList)):
-        message += chr(int(byteList[i],2))
-    print(message)
-    
+    #3 bytes to store size of message. 16,777,215 bytes(ASCII characters) max
+    binaryLength = payloadByteLength | (1<<24) #Binary representation of length, need the 1 to keep in onder
 
-def binaryDataToList(data):
-    binaryDataList = []
-
-    for char in data:
+    j = 0
+    for i in bin(binaryLength):
+        if j > 2: #To Ignore 0b1.... 
+            payloadBitList.append(int(i))
+        j += 1
+        
+    for char in characterList:
         for i in range(8):
             filter = 1 << (7-i) #Creating a filter starting with leftmost digit.
             result = char & filter #Getting leftmost digit with filter
             bit = result >> (7-i) #Removing trailing 0s
-            binaryDataList.append(bit)
-    return binaryDataList
+            payloadBitList.append(bit)
+    return payloadBitList
 
 def replaceData(imageData, payload):
-    binaryDataList = binaryDataToList(payload) #Is this line necessary? 
-
-    if len(binaryDataList) <= lengthOfListOfList(imageData): #Check that input is smaller than image length
+    payloadBitList = payloadToBinaryList(payload)
+    
+    if len(payloadBitList) <= lengthOfListOfList(imageData): #Todo, just *3 remove method
         j = 0
-        for i in range(len(binaryDataList)//3):
+        for i in range(len(payloadBitList)//3):
             for rgb in range(3):
-
                 imageData[i][rgb] &= 0b11111110 #0 the least significant bit
-                imageData[i][rgb] |= binaryDataList[j] #Replace with new bit.
+                imageData[i][rgb] |= payloadBitList[j] #Replace with new bit.
                 j = j+1
         return(imageData)
     else:
@@ -88,15 +62,55 @@ def saveImage(data, size, mode):
     secretImage.putdata(data)
     secretImage.save("secret.bmp", "BMP")
 
+
+
+def lsbToCharacters(inList): 
+    messageLength = 0
+    tempList = []
+    for i in range(24):
+        #tempList.append(inList[i])
+        # Can do something like x = x << 0 |i for each iteration. So don't have to think about strings. Can also do this below I believe. 
+        messageLength = (messageLength << 1) | inList[i] 
+        
+    bitCount = 0
+    outList = []
+    byte = 0
+    for bit in range(24, (messageLength*8) + 24): #TODO use length 
+        if bitCount < 8:
+            byte = (byte << 1) | inList[bit]
+            bitCount += 1
+        if bitCount == 8:
+            outList.append(chr(byte))
+            byte = 0
+            bitCount = 0
+    return(outList)
+
+def decode(path): #Supposed to store length
+    data = loadImage(path)
+    dataList = list(map(list, data)) #Python has funky tuple rules
+    extractedList = []
+    
+    for i in range(len(dataList)):
+        for rgb in range(3):
+            extractedList.append(dataList[i][rgb] & 1)
+            
+    byteList = lsbToCharacters(extractedList)
+    
+    message = ''
+    for i in range(len(byteList)):
+        message += byteList[i]
+    print(message)
+    
+    
 if __name__ == "__main__":
-    imageData = loadImageData("glasgow.bmp")
+    imageData = loadImage("glasgow.bmp")
     size = imageData.size
     mode = imageData.mode
     dataList = list(map(list, imageData)) #Python has funky tuple rules
         
-    newData = replaceData(dataList, stringToNumberList("Hello my name is Harry"))
+    newData = replaceData(dataList, "Hello")
 
     dataTuples = list(map(tuple,newData))
     saveImage(dataTuples, size, mode)
 
-    decode("secret.bmp", 200)
+    decode("secret.bmp")
